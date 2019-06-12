e7a654e497b4675df613985786751558ca5376cd
Humphrey Ahn <ahnsv@bc.edu>
2019-06-12 21:13:35 +0900

    feat: logo add

--- components/Menu.js
+++ components/Menu.js
@@ -13,9 +13,21 @@ const Menu = () => (
         }}
     >
         <div className="title" style={{marginRight: 'auto'}}>
-            <h1>
-                Title
-            </h1>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 79 36.78" style={{width: '70px', fill: 'white'}}>
+                <defs>
+                </defs>
+                <title>자산 2</title>
+                <g id="레이어_2" data-name="레이어 2">
+                    <g id="레이어_1-2" data-name="레이어 1">
+                        <path class="cls-1"
+                              d="M20.56,30.5A37.82,37.82,0,0,0,26,26a37.82,37.82,0,0,0,4.49-5.45,36.22,36.22,0,0,0,3.39-6.24A36.57,36.57,0,0,0,36.78,0H0V36.78a36.57,36.57,0,0,0,14.32-2.89A36.22,36.22,0,0,0,20.56,30.5Z"/>
+                        <polygon class="cls-1"
+                                 points="58.92 0 58.92 36.58 58.92 36.78 59.12 36.78 79 36.78 79 36.78 79 0 58.92 0"/>
+                        <polygon class="cls-1"
+                                 points="37.71 36.78 37.91 36.78 57.79 36.78 57.99 36.78 57.99 36.58 57.99 0 37.71 0 37.71 36.58 37.71 36.78"/>
+                    </g>
+                </g>
+            </svg>
         </div>
         <ul className={`menu-items`}>
             <li>
